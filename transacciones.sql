e039c952cf84a7d78c654c575e697fabf15e241b
Julian-E-Mieres <149446349+Julian-E-Mieres@users.noreply.github.com>
2023-10-31 15:19:05 -0300

    Update transacciones.sql

--- transacciones.sql
+++ transacciones.sql
@@ -102,6 +102,46 @@ BEGIN CATCH -- SI ALGO FALLA VENDRA AQUI
 	SELECT ERROR_MESSAGE() -- MOSTRAMOS EL MENSAJE DE ERROR
 	ROLLBACK TRAN -- VOLVEMOS HACIA ATRAS PARA MANTENER LA CONSISTENCIA DE LOS DATOS
 END CATCH
+
+
+
+
+-----------------------------------------------
+--- CASO Transacción Anidada 
+-- USE base_consorcio;
+
+-- CREACION DE TRANSACCIONES
+BEGIN TRY -- INICIAMOS EL BEGIN TRY PARA LUEGO COLOCAR LA LOGICA DENTRO Y ASEGURARNOS DE QUE SI ALGO FALLA IRA POR EL CATCH
+	BEGIN TRAN -- COMENZAMOS LA TRANSACCION
+	INSERT INTO administrador(apeynom, viveahi, tel, sexo, fechnac) -- UN INSERT A LA TABLA QUE QUEREMOS
+	VALUES ('pablito clavounclavito', 'S', '37942222', 'M', '01/01/1996') -- LOS VALORES A INGRESAR A LA TABLA
+
+	INSERT INTO consorcio(idprovincia, idlocalidad, idconsorcio, nombre, direccion, idzona, idconserje, idadmin)
+	VALUES (1, 1, 3, 'EDIFICIO-111', 'PARAGUAY N 999', 5, 100, 1) -- GENERAMOS UN ERROR INGRESANDO EL ID PROVINCIA 999 QUE NO EXISTE.
+
+	-------------------------
+	BEGIN TRAN -- COMENZAMOS UNA TRANSACCION ANIDADA
+		UPDATE consorcio SET nombre = 'EDIFICIO-222'; -- ACTUALIZAMOS EL REGISTRO DE CONSORCIO QUE CARGAMOS ANTES
+		INSERT INTO gasto(idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe) 
+			VALUES (1,1,1,6,'20130616',5,608.97) -- INSERT A LA TABLA GASTO, DEBE DAR UN ERROR POR TIPO DE GASTO
+	COMMIT TRAN -- FINALIZAMOS UNA TRANSACCION ANIDADA
+	------------------------
+
+	INSERT INTO gasto(idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
+	VALUES (1,1,1,6,'20130616',5,608.97)
+	INSERT INTO gasto(idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
+	VALUES (1,1,1,6,'20130616',2,608.97)
+	INSERT INTO gasto(idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
+	VALUES (1,1,1,6,'20130616',3,608.97)
+
+	COMMIT TRAN -- SI TODO FUE EXITOSO FINALIZAMOS LA TRANSACCION
+END TRY
+BEGIN CATCH -- SI ALGO FALLA VENDRA AQUI
+	SELECT ERROR_MESSAGE() -- MOSTRAMOS EL MENSAJE DE ERROR
+	ROLLBACK TRAN -- VOLVEMOS HACIA ATRAS PARA MANTENER LA CONSISTENCIA DE LOS DATOS
+END CATCH
+
+-----------------------------------------------
 -----------------------------------------------
 
 
